2077e05b4978eeb8759f42bef66d6f1e3f03d444
RabbanMehamood <shaik.mehamood786@gmail.com>
2024-06-28 22:01:28 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AboutMe from './components/AboutMe/AboutMe';
+import Contact from  './components/Contact/Contact';
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <AboutMe />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,27 @@
+import './AboutMe.css'
+
+
+
+function AboutMe() {
+    return ( 
+        <div className="container">
+         <div className='half'>
+           <h1 className='welcome-heading'>Welcome to My Portfolio</h1>
+           <h4 class="zoom-on-hover">About Me</h4>
+           <p class="zoom-on-hover">Hello! My name is Mehamood Rabban, and I am delighted to welcome you to my portfolio. I am a passionate and dedicated individual with a keen interest in web development, particularly using React. My goal is to create intuitive and dynamic user experiences that captivate and engage users.
+               With a strong foundation in modern web technologies, I have been able to craft and contribute to a variety of projects that demonstrate my ability to blend creativity with functionality. My journey in web development began a few years ago and has been marked by continuous learning, creative problem-solving, and a commitment to excellence.</p>
+           <h4 class="zoom-on-hover">My Philosophy</h4>
+           <p class="zoom-on-hover">I believe that the intersection of creativity and technology offers limitless possibilities. My approach is driven by a desire to not only meet but exceed expectations, ensuring that every project I work on is both visually appealing and functionally robust. I am constantly exploring new techniques and staying up-to-date with industry trends to bring fresh and innovative ideas to life.</p> 
+
+         </div>
+         <div className='half'>
+         
+            
+         </div>
+        </div>
+     );
+}
+ 
+export default AboutMe;
+
+
